37e787a90bdfc4575d6f54145dc994802d4b5039
ng <nicolas@nghs.fr>
2007-04-13 11:31:18 +0000

    fin du port pour windows
    git-svn-id: file:///Users/ng/svn-local/to_share/gemmes@70 a6041290-3ed1-4cff-9e99-c9f6f095c445

--- win_port.c
+++ win_port.c
@@ -1,6 +1,26 @@
 #include <windows.h>
 #include <winbase.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#define M_MAXSIZE 1024
+
+/* implementation simple de getline() pour windows */
+unsigned int getline(char **lineptr, unsigned int *n, FILE *stream)
+{
+    if(!*lineptr)
+    {
+	*lineptr = (char*)malloc(M_MAXSIZE);
+	*n = M_MAXSIZE;
+    }
+
+    if(!fgets(*lineptr,  M_MAXSIZE, stream))
+	return (unsigned int)-1;
+    
+    return strlen(*lineptr);
+}
+
 
 void usleep(unsigned int s)
 {
@@ -8,8 +28,3 @@ void usleep(unsigned int s)
 }
 
 
-unsigned int getline(char **lineptr, unsigned int *n, FILE *stream)
-{
-	fputs("getline() not yet implemented\n", stderr);
-	return (unsigned int)-1;
-}
